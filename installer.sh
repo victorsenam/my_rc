09aafc102837cb3da101b771652a8f011268cc31
Victor Sena Molero <victorsenam@gmail.com>
2014-06-01 21:38:08 -0300

    Installer Ajustado

--- installer.sh
+++ installer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd files
-cp --target-directory=$HOME * -r
-echo "Configuracoes Copiadas para $HOME."
+echo "Isso vai apagar todas as suas configuracoes atuais. Voce tem certeza? [y/N]"
+read confirm
+
+case $confirm in
+y|Y)
+    cd files
+    cp --target-directory=$HOME ./ -r
+    echo "Configuracoes Copiadas para $HOME."
+    break;;
+*)
+    echo "Operacao Cancelada"
+esac
